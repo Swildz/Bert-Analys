be5623dadc1cc46147dea9e32a7d8349304e5a3c
Swildz <satupercobaan12@gmail.com>
2025-04-21 14:10:59 +0700

    flask

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html', title='Halaman Utama')
+
+@app.route('/about')
+def about():
+    return '<h1>Tentang Kami</h1><p>Ini adalah halaman tentang kami.</p>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
